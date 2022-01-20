568facc61cfc19d905babf9d3c2021b1dc189e51
saiphyozinko <40524304@live.napier.ac.uk>
2022-01-20 16:24:32 +0630

    new design look modified

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
 
-w = json.load(open("worldl.json"))
+w = json.load(open("static/worldl.json"))
 
 @app.route("/")
 def index():
@@ -11,7 +11,7 @@ def index():
 
 @app.route("/country/<i>")
 def country(i):
-    return render_template("country.html", c = w[int(i)])
+    return render_template("country.html", c = w[int(i)], next = 1+int(i))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,1 +1,40 @@
-document.body.append("This comes from JavaScript")
+fetch('static/worldl.json')
+   .then(r=>r.json())
+   .then(r=>{
+       for(let c of r){
+           let d = document.createElement('div');
+           d.innerText = c.name;
+           d.classList.add('countryIndex');
+           d.onclick = ()=>{
+               document.getElementById('content').innerHTML = `
+               <div class="tbone">
+               <table class="cty-list">
+               <tr><th> Name:</th><td>${c.name}</td></tr>
+               <tr><th>Id: </th><td>${c.id}</td></tr>
+               <tr><th>Capital: </th><td>${c.capital}</td></tr>
+               <tr><th>Continent: </th><td>${c.continent}</td></tr>
+               <tr><th>Area: </th><td>${c.area}</td></tr>
+               <tr><th>GDP: </th><td>${c.gdp}</td></tr>
+               <tr><th>Population: </th><td>${c.population}</td></tr>
+               </table>
+               </div>
+                <div class="tbtwo">
+                    <img src=${c.flag}>
+                </div>
+               
+               `;
+           }
+           document.getElementById('countryList').append(d);
+       }
+       document.querySelector('select').onchange=()=>{
+        //    console.log(document.querySelector("select").value)
+           let letter = document.querySelector('select').value
+           for(let n of document.querySelectorAll('#countryList div')){
+            if (n.innerText.startsWith(letter)){
+                n.classList.remove('hide');
+            }else{
+                n.classList.add('hide');
+            }
+        } 
+       }
+    })
